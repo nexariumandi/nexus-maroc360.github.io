4ccbb23321d72dead66506746424b24f923db95b
nexariumandi <hassine.ah@outlook.fr>
2025-04-09 00:53:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    document.getElementById('webcam').srcObject = stream;
-  })
-  .catch(err => {
-    console.error("Caméra inaccessible :", err);
-  });
-
-document.getElementById('explore-btn').addEventListener('click', () => {
-  alert("Un ancien fragment vous est révélé...");
-});
+console.log("WebApp I KNOW IT ALL prête. Modules actifs.");
